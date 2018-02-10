d2d2d7f39d059478a215977e9449de394a148dcb
mohameddhameem <32428908+mohameddhameem@users.noreply.github.com>
2018-02-10 20:35:58 +0800

    Integrating front end with backend clips code

---
+++ ExpertRecommendationSystem/DebugRouter.cs
@@ -0,0 +1,40 @@
+﻿using Mommosoft.ExpertSystem;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpertRecommendationSystem
+{
+    class DebugRouter : Router
+    {
+        public override string Name
+        {
+            get { return "DebugRouter"; }
+        }
+
+        public override int Priority
+        {
+            get { return 10; }
+        }
+
+        public override bool Query(string logicalName)
+        {
+            return (logicalName.Equals("wwarning") ||
+                   logicalName.Equals("werror") ||
+                   logicalName.Equals("wtrace") ||
+                   logicalName.Equals("wdialog") ||
+                   logicalName.Equals("wclips") ||
+                   logicalName.Equals("wdisplay"));
+        }
+
+        public override int Print(string logicalName, string message)
+        {
+            Debug.Write(message);
+            Console.Write(message);
+            return 1;
+        }
+    }
+}
--- ExpertRecommendationSystem/MainForm.cs
+++ ExpertRecommendationSystem/MainForm.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using Mommosoft.ExpertSystem;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,13 +16,132 @@ namespace ExpertRecommendationSystem
 {
     public partial class MainForm : Form
     {
+        private Mommosoft.ExpertSystem.Environment clipsEnvironment = new Mommosoft.ExpertSystem.Environment();
         public MainForm()
         {
+            ConsoleTraceListener tl = new ConsoleTraceListener();
             InitializeComponent();
+            clipsEnvironment.AddRouter(new DebugRouter());
+            var path = @"ClipsScript.clp";
+            clipsEnvironment.Load(path);
+            clipsEnvironment.Reset();
         }
         private void OnClickButton(object sender, EventArgs e)
         {
+            Button button = sender as Button;
+            // Get the state-list.
+            String evalStr = "(find-all-facts ((?f state-list)) TRUE)";
+            using (FactAddressValue f = (FactAddressValue)((MultifieldValue)clipsEnvironment.Eval(evalStr))[0])
+            {
+                string currentID = f.GetFactSlot("current").ToString();
 
+                if (button.Tag.Equals("Next"))
+                {
+                    if (GetCheckedChoiceButton() == null) { clipsEnvironment.AssertString("(next " + currentID + ")"); }
+                    else
+                    {
+                        clipsEnvironment.AssertString("(next " + currentID + " " +
+                                           (string)GetCheckedChoiceButton().Tag + ")");
+                    }
+                    NextUIState();
+                }
+                else if (button.Tag.Equals("Restart"))
+                {
+                    clipsEnvironment.Reset();
+                    NextUIState();
+                }
+                else if (button.Tag.Equals("Prev"))
+                {
+                    clipsEnvironment.AssertString("(prev " + currentID + ")");
+                    NextUIState();
+                }
+            }
+        }
+        private void NextUIState()
+        {
+            nextButton.Visible = false;
+            prevButton.Visible = false;
+            choicesPanel.Controls.Clear();
+            clipsEnvironment.Run();
+
+            // Get the state-list.
+            String evalStr = "(find-all-facts ((?f state-list)) TRUE)";
+            using (FactAddressValue allFacts = (FactAddressValue)((MultifieldValue)clipsEnvironment.Eval(evalStr))[0])
+            {
+                string currentID = allFacts.GetFactSlot("current").ToString();
+                evalStr = "(find-all-facts ((?f UI-state)) " +
+                               "(eq ?f:id " + currentID + "))";
+            }
+
+            using (FactAddressValue evalFact = (FactAddressValue)((MultifieldValue)clipsEnvironment.Eval(evalStr))[0])
+            {
+                string state = evalFact.GetFactSlot("state").ToString();
+                if (state.Equals("initial"))
+                {
+                    nextButton.Visible = true;
+                    nextButton.Tag = "Next";
+                    nextButton.Text = "Next";
+                    prevButton.Visible = false;
+                }
+                else if (state.Equals("final"))
+                {
+                    nextButton.Visible = true;
+                    nextButton.Tag = "Restart";
+                    nextButton.Text = "Restart";
+                    prevButton.Visible = false;
+                }
+                else
+                {
+                    nextButton.Visible = true;
+                    nextButton.Tag = "Next";
+                    prevButton.Tag = "Prev";
+                    prevButton.Visible = true;
+                }
+
+
+
+                using (MultifieldValue validAnswers = (MultifieldValue)evalFact.GetFactSlot("valid-answers"))
+                {
+                    String selected = evalFact.GetFactSlot("response").ToString();
+                    for (int i = 0; i < validAnswers.Count; i++)
+                    {
+                        RadioButton rb = new RadioButton();
+                        rb.Text = (SymbolValue)validAnswers[i];
+                        rb.Tag = rb.Text;
+                        rb.Visible = true;
+                        rb.Location = new Point(10, 20 * (i + 1));
+                        choicesPanel.Controls.Add(rb);
+                    }
+                }
+                messageLabel.Text = GetString((SymbolValue)evalFact.GetFactSlot("display"));
+            }
+        }
+        private void ShowChoices(bool visible)
+        {
+            foreach (Control control in choicesPanel.Controls)
+            {
+                control.Visible = visible;
+            }
+        }
+        private RadioButton GetCheckedChoiceButton()
+        {
+            foreach (RadioButton control in choicesPanel.Controls)
+            {
+                if (control.Checked)
+                {
+                    return control;
+                }
+            }
+            return null;
+        }
+        private string GetString(string name)
+        {
+            return ExpSysResources.ResourceManager.GetString(name);
+        }
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            NextUIState();
         }
     }
 }
